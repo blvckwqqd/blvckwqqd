7910a11ff29b77eb7d1dcb721fab3b1d7db4b2e5
blvckwqqd <mishagin.2012@mail.ru>
2022-01-29 14:34:39 +0500

    master

---
+++ server/db.js
@@ -0,0 +1,13 @@
+
+const {sequalize, Sequelize} = require('sequelize');
+
+module.exports = new Sequelize(
+    process.env.DB_NAME, // Имя Бд
+    process.env.DB_USER, // Имя пользователя
+    process.env.DB_PASSWORD, // Пароль
+    {
+        dialect:'postgres',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT
+    }
+);
